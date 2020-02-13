3c38f83c8d278705bc19c778099ba26499732ec7
GraceeFlower <1491644103@qq.com>
2020-02-13 23:13:40 +0800

    [WM] refactor: delete the function turnLeft & turnRight in Direction

--- marsrover/src/main/java/com/Car.java
+++ marsrover/src/main/java/com/Car.java
@@ -2,7 +2,7 @@ package com;
 
 public class Car {
     public static void main(String[] args) {
-        Direction car = new South('S');
+        South car = new South('S');
 
         System.out.println("小车左转之后的方位是：" + car.turnLeft());
         System.out.println("小车右转之后的方位是：" + car.turnRight());
--- marsrover/src/main/java/com/Direction.java
+++ marsrover/src/main/java/com/Direction.java
@@ -7,36 +7,6 @@ public class Direction {
     this.direction = direction;
   }
 
-  public Direction turnRight() {
-    switch (direction) {
-      case 'N':
-        return new Direction('E');
-      case 'S':
-        return new Direction('W');
-      case 'E':
-        return new Direction('N');
-      case 'W':
-        return new Direction('S');
-      default:
-        throw new IllegalArgumentException();
-    }
-  }
-
-  public Direction turnLeft() {
-    switch (direction) {
-      case 'N':
-        return new Direction('W');
-      case 'S':
-        return new Direction('E');
-      case 'E':
-        return new Direction('N');
-      case 'W':
-        return new Direction('S');
-      default:
-        throw new IllegalArgumentException();
-    }
-  }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
