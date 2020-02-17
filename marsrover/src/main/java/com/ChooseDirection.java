310bba9aff8177da2ddaf17ccddd99a0e691172c
GraceeFlower <1491644103@qq.com>
2020-02-17 13:19:36 +0800

    [WM] refactor: delete the turnLeft & turnRight function in Direction clas

--- marsrover/src/main/java/com/ChooseDirection.java
+++ marsrover/src/main/java/com/ChooseDirection.java
@@ -2,6 +2,6 @@ package com;
 
 public interface ChooseDirection {
 
-    public abstract Direction turnLeft();
-    public abstract Direction turnRight();
+    Direction turnLeft();
+    Direction turnRight();
 }
